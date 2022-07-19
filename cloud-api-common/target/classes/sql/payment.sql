3f620a61f0288c1de0df7d30d3c53a67557bb431
zxxcth201314 <862100986@qq.com>
2022-07-19 17:53:41 +0800

    learn  Hystrix

--- cloud-api-common/target/classes/sql/payment.sql
+++
@@ -1,9 +0,0 @@
--- ----------------------------
--- Table structure for payment
--- ----------------------------
-DROP TABLE IF EXISTS `payment`;
-CREATE TABLE `payment` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `serial` varchar(200) DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
